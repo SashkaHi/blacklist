4b6110e11c99de1cb8678272d2d31f1b2eb9d81a
SashkaHi <Drys123@list.ru>
2025-01-21 19:12:26 +0300

    first commit

---
+++ blacklistproject/src/App.tsx
@@ -0,0 +1,150 @@
+import React from 'react';
+import Modal from "./components/modal/Modal.tsx";
+
+const App: React.FC = () => {
+    return (
+        <div>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deserunt dolorum, error explicabo illum
+                ipsam itaque laboriosam molestiae nihil, non officiis quaerat sit, voluptate. Labore neque possimus
+                suscipit vel vitae?</p>
+            <Modal/>
+        </div>
+    );
+};
+
+export default App;
---
+++ blacklistproject/src/components/modal/Modal.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import "./modal.module.scss"
+
+const Modal: React.FC = () => {
+    return (
+        <div className="modal">
+        <div className="content">
+
+        </div>
+        </div>
+    );
+};
+
+export default Modal;
